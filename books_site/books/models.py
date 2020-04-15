2bfce40a469fe170c9ab6a05c2c0806cc64bf33f
soundreaper <soundreaper@gmail.com>
2020-04-14 20:53:58 -0700

    complete project

--- books_site/books/models.py
+++ books_site/books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    birth_date = models.DateField('date born')
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    num_pages = models.IntegerField()
+    date_published = models.DateField('date published')
+    author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
--- books_site/books/views.py
+++ books_site/books/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def home(request):
+  context = {
+    'books': Book.objects.all()
+  }
+  return render(request, 'home.html', context)
+
+def detail(request, book_id):
+  context = {
+    'book': Book.objects.get(id=book_id)
+  }
+  return render(request, 'detail.html', context)
